5e8af2f6e063f2b34a7e0130d1467ab88b6dde61
DiukovaKs <diukova.ks@gmail.com>
2019-07-15 01:04:16 +0700

    ДЗ JS_Objects JS_Arrays (исправления от 11.07.2019)

--- Lecture3/src/Diukova/JS/Arrays/Array.js
+++ Lecture3/src/Diukova/JS/Arrays/Array.js
@@ -1,69 +1,54 @@
 var array = [5, 6, 9, 1, 15, 3, 11, 27, 8, 16, 30, 45, 4];
 
-function arraySort(array) {
-    array.sort(descendingSort);
-
-    function descendingSort(a, b) {
+function sortArray(array) {
+    array.sort(function (a, b) {
         return b - a;
-    }
+    });
 }
 
-function subArrayFromHead(array) {
+function getSubArrayFromHead(array) {
     return array.slice(0, 5);
 }
 
-function subArrayFromTail(array) {
+function getSubArrayFromTail(array) {
     return array.slice(array.length - 5);
 }
 
-function arrayEvenElementsSum(array) {
-    function evenNumbersFilter(array) {
-        return array.filter(function (elem) {
-            return (elem % 2) === 0;
-        });
-    }
-
-    var evenArr = evenNumbersFilter(array);
-    return evenArr.reduce(function (sum, current) {
+function getArrayEvenElementsSum(array) {
+    return array.filter(function (elem) {
+        return (elem % 2) === 0;
+    }).reduce(function (sum, current) {
         return sum + current;
     }, 0);
 }
 
-arraySort(array);
+sortArray(array);
 console.log(array.join(","));
 
-var subArrayHead = subArrayFromHead(array);
+var subArrayHead = getSubArrayFromHead(array);
 console.log(subArrayHead.join(","));
 
-var subArrayTail = subArrayFromTail(array);
+var subArrayTail = getSubArrayFromTail(array);
 console.log(subArrayTail.join(","));
 
-var arrayEvenElementsSumResult = arrayEvenElementsSum(array);
+var arrayEvenElementsSumResult = getArrayEvenElementsSum(array);
 console.log(arrayEvenElementsSumResult);
 
 var bigArray = [];
 
 for (var i = 0; i < 100; ++i) {
-    bigArray[i] = i + 1;
+    bigArray.push(i + 1);
 }
 
-function squaredArray(array) {
-    var result = [];
-
-    function evenNumbersFilter(array) {
-        return array.filter(function (elem) {
-            return (elem % 2) === 0;
-        });
-    }
-
-    var filteredArray = evenNumbersFilter(array);
-    filteredArray.forEach(function (item, i, filteredArray) {
-        result[i] = Math.pow(filteredArray[i], 2);
+function getSquareOfArrayEvenElements(array) {
+    return array.filter(function (elem) {
+        return (elem % 2) === 0;
+    }).map(function (item) {
+        return Math.pow(item, 2);
     });
-    return result;
 }
 
 console.log(bigArray.join(","));
 
-var squared = squaredArray(bigArray);
+var squared = getSquareOfArrayEvenElements(bigArray);
 console.log(squared.join(","));
