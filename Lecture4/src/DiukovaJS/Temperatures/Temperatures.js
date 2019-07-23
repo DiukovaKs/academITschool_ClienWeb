f7ce01b7c9c8edb47377c853e90ec020dc5acadf
DiukovaKs <diukova.ks@gmail.com>
2019-07-24 00:55:25 +0700

    ДЗ JS_Temperatures JS_ToDoList (замечания от 23.07.2019)

--- Lecture4/src/DiukovaJS/Temperatures/Temperatures.js
+++ Lecture4/src/DiukovaJS/Temperatures/Temperatures.js
@@ -7,15 +7,15 @@ document.addEventListener("DOMContentLoaded", function () {
     convertButton.addEventListener("click", function () {
         var temperature = Number(initTemperature.value);
         var resTemperature;
+
         if (isNaN(temperature) || initTemperature.value === "") {
             alert("Type temperature!");
             resTemperature = "";
             return;
         }
         if (radioButton[0].checked) {
-            resTemperature = temperature + 278.15;
-        }
-        if (radioButton[1].checked) {
+            resTemperature = temperature + 273.15;
+        } else {
             resTemperature = temperature * 9 / 5 + 35;
         }
         return result.textContent = resTemperature;
--- Lecture4/src/DiukovaJS/ToDoList/ToDoList.js
+++ Lecture4/src/DiukovaJS/ToDoList/ToDoList.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var toDoButton = document.getElementById("addButton");
     var toDoList = document.getElementById("toDoList");
     var validationMessage = document.querySelector(".validationMessage");
+
     toDoButton.addEventListener("click", function () {
         var newText = toDoText.value;
         if (newText === "") {
@@ -11,28 +12,34 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         validationMessage.style.display = "none";
         var li = document.createElement("li");
-        li.innerHTML = "<span></span><button type = 'button'>X</button><button type = 'button'>Edit</button >";
-        li.children[0].textContent = newText + "   ";
+
+        li.innerHTML = "<span class='span'></span><button class='button' type='button'>X</button><button class='button' type='button'>Edit</button >";
+        li.children[0].textContent = newText;
 
         li.children[1].addEventListener("click", function () {
             toDoList.removeChild(li);
         });
+
         li.children[2].addEventListener("click", function () {
             var li2 = document.createElement("li");
-            li2.innerHTML = "<input type='text'><button type = 'button'>X</button><button type = 'button'>cancel</button><button type = 'button'>save</button >";
+
+            li2.innerHTML = "<input id='inputEdit' type='text'><button class='button' type='button'>X</button><button class='button' type='button'>cancel</button><button class='button' type='button'>save</button >";
             li2.children[0].value = newText;
             li.style.display = "none";
             toDoList.insertBefore(li2, li);
+
             li2.children[1].addEventListener("click", function () {
                 toDoList.removeChild(li2);
             });
+
             li2.children[2].addEventListener("click", function () {
                 toDoList.removeChild(li2);
                 li.style.display = "block";
             });
+
             li2.children[3].addEventListener('click', function () {
                 newText = li2.children[0].value;
-                li.children[0].textContent = newText + "   ";
+                li.children[0].textContent = newText;
                 toDoList.removeChild(li2);
                 li.style.display = "block";
             })
